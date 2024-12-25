a79eb4fc73e8297ca61c7f2feaa432831649ac90
tillber <tillber@kth.se>
2024-12-25 17:34:54 +0100

    Refactor

--- problems/monopol/main.py
+++ problems/monopol/main.py
@@ -1,4 +1,3 @@
-import sys
 from decimal import Decimal
 
 
@@ -7,24 +6,14 @@ def probability(distances: tuple) -> Decimal:
     for dist in distances:
         for i in range(1, 7):
             for j in range(1, 7):
-                if (i + j) == dist:
+                if (i + j) == int(dist):
                     cnt += 1
     return cnt/36
 
 
 def run():
-    hotels_cnt = None
-    hotels_dist = ()
-
-    for i, line in enumerate(sys.stdin):
-        if i == 0:
-            # Collect first line as the amount of the opponents hotels
-            hotels_cnt = int(line)
-            continue
-        # Collect the second line as the distances to each of the oppopents hotels
-        hotels_dist = tuple(int(d) for d in str.split(line.strip(), " "))
-        break
-
+    input()  # Input unused amount of opponents hotels
+    hotels_dist = tuple(input().split())  # Input distances to opponents hotels
     print(probability(hotels_dist))
 
 
