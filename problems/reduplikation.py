922ff4afef007e36ccb3bb9efd6e7e1d777fd7c2
tillber <tillber@kth.se>
2024-12-25 16:45:48 +0100

    Rename to reduplikation.py

---
+++ problems/reduplikation.py
@@ -0,0 +1,17 @@
+# https://open.kattis.com/problems/reduplikation
+
+import sys
+
+word = ""
+times = 0
+
+for i, line in enumerate(sys.stdin):
+    if i == 0:
+        # Collect first line as the word (to reduplicate)
+        word = line.strip()
+        continue
+    # Collect the second line as the amount of times to reduplicate the given word.
+    times = int(line)
+    break
+
+print(word*times)
