2cfe1d2700a48d6e93048097989e96433a98bd1e
tillber <tillber@kth.se>
2024-12-26 14:49:13 +0100

    Fix fodelsedagsmemorisering

--- problems/fodelsedagsmemorisering.py
+++ problems/fodelsedagsmemorisering.py
@@ -4,9 +4,9 @@ bday_map = {}
 for _ in range(int(input())):
     s, c, d = input().split()
     (_, c2, _) = bday_map.get(d, (None, None, None))
-    if c2 is None or c > c2:
-        bday_map[d] = (s, c, d)
+    if c2 is None or int(c) > c2:
+        bday_map[d] = (s, int(c), d)
 
 print(len(bday_map))
-for f in list(bday_map.values()):
+for f in sorted(bday_map.values()):
     print(f[0])
